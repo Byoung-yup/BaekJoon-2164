c90afd4bcc7b1da9d8e63bf41d77d354032a702c
Dev_Ozzy <20170926@vision.hoseo.edu>
2023-02-01 18:49:45 +0900

    commit

--- BaekJoon#2164/main.swift
+++ BaekJoon#2164/main.swift
@@ -4,8 +4,20 @@
 //
 //  Created by 김병엽 on 2023/02/01.
 //
-
+// Reference: https://seolhee2750.tistory.com/93
 import Foundation
 
-print("Hello, World!")
+let N = Int(readLine()!)!
+var queue = Array(1...N)
+var tmp = 0
 
+if N == 1 { print(1) }
+else {
+    while true {
+        queue[tmp] = 0
+        queue.append(queue[tmp + 1])
+        queue[tmp + 1] = 0
+        if queue[queue.count - 2] == 0 { print(queue.last!); break }
+        tmp += 2
+    }
+}
